a5dce6ac61a0f72b60f1738558038224a1760912
Torikul islam <toriqulislam142@gmail.com>
2022-11-14 21:36:22 +0600

    modified navbar

--- src/components/shared/Footer/Footer.js
+++ src/components/shared/Footer/Footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './Footer.css'
 const Footer = () => {
     return (
-        <div>
+        <footer>
 
-        </div>
+        </footer>
     );
 };
 
--- src/components/shared/Navbar/Navbar.js
+++ src/components/shared/Navbar/Navbar.js
@@ -1,56 +1,76 @@
 import React from 'react';
 import './Navbar.css'
 import { IoIosArrowDown } from 'react-icons/io'
+import { BsFacebook, BsTwitter, BsYoutube, BsPinterest, BsFillPeopleFill } from 'react-icons/bs'
+import {BiSearchAlt2} from 'react-icons/bi'
 const Navbar = () => {
     return (
         <nav>
-            <div className="logo">Timu Blog</div>
-            <div className="navlinks">
-                <div className="dropdown-container">
-                    <div className='btn-arrow'><button className="dropdown-btn">BLOGS</button> <IoIosArrowDown className='drop-icon' /></div>
-                    <div className="dropdown-content">
-                        <a href="">Technology</a>
-                        <a href="">Sports</a>
-                        <a href="">Food</a>
-                        <a href="">Health</a>
-                    </div>
+            <div className="topbar-container">
+                <div className="topbar-links">
+                    <a href="">Home</a>
+                    <a href="">About Us</a>
+                    <a href="">Privacy Policy</a>
+                    <a href="">Contact Us</a>
                 </div>
-                <div className="dropdown-container">
-                    <div className='btn-arrow'><button className="dropdown-btn">SHOP</button> <IoIosArrowDown className='drop-icon' /></div>
-                    <div className="dropdown-content">
-                        <div className="dropdown-content">
-                            <a href="">Technology</a>
-                            <a href="">Sports</a>
-                            <a href="">Food</a>
-                            <a href="">Health</a>
-                        </div>
-                    </div>
-                </div>
-                <div className="dropdown-container">
-                    <div className='btn-arrow'><button className="dropdown-btn">NEWS</button> <IoIosArrowDown className='drop-icon' /></div>
-                    <div className="dropdown-content">
-                        <div className="dropdown-content">
-                            <a href="">Technology</a>
-                            <a href="">Sports</a>
-                            <a href="">Food</a>
-                            <a href="">Health</a>
-                        </div>
-                    </div>
-                </div>
-                <div className="dropdown-container">
-                    <div className='btn-arrow'><button className="dropdown-btn">FEATURES</button> <IoIosArrowDown className='drop-icon' /></div>
-                    <div className="dropdown-content">
-                        <div className="dropdown-content">
-                            <a href="">Technology</a>
-                            <a href="">Sports</a>
-                            <a href="">Food</a>
-                            <a href="">Health</a>
-                        </div>
-                    </div>
+                <div className="topbar-socials">
+                    <a href=""><BsFacebook /></a>
+                    <a href=""><BsTwitter /></a>
+                    <a href=""><BsYoutube /></a>
+                    <a href=""><BsPinterest /></a>
                 </div>
             </div>
-            <div className="nav-btn">
-                login
+
+
+            <div className="navbar-container">
+                <div className="navlinks">
+                    <a className='logo' href="">TIMU</a>
+                    <div className="dropdown-container">
+                        <div className='btn-arrow'><button className="dropdown-btn">BLOGS</button> <IoIosArrowDown className='drop-icon' /></div>
+                        <div className="dropdown-content">
+                            <a href="">Technology</a>
+                            <a href="">Sports</a>
+                            <a href="">Food</a>
+                            <a href="">Health</a>
+                        </div>
+                    </div>
+                    <div className="dropdown-container">
+                        <div className='btn-arrow'><button className="dropdown-btn">SHOP</button> <IoIosArrowDown className='drop-icon' /></div>
+                        <div className="dropdown-content">
+                            <div className="dropdown-content">
+                                <a href="">Technology</a>
+                                <a href="">Sports</a>
+                                <a href="">Food</a>
+                                <a href="">Health</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="dropdown-container">
+                        <div className='btn-arrow'><button className="dropdown-btn">NEWS</button> <IoIosArrowDown className='drop-icon' /></div>
+                        <div className="dropdown-content">
+                            <div className="dropdown-content">
+                                <a href="">Technology</a>
+                                <a href="">Sports</a>
+                                <a href="">Food</a>
+                                <a href="">Health</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="dropdown-container">
+                        <div className='btn-arrow'><button className="dropdown-btn">FEATURES</button> <IoIosArrowDown className='drop-icon' /></div>
+                        <div className="dropdown-content">
+                            <div className="dropdown-content">
+                                <a href="">Technology</a>
+                                <a href="">Sports</a>
+                                <a href="">Food</a>
+                                <a href="">Health</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="search-container">
+                   <BiSearchAlt2 className='search-icon' />
+                </div>
             </div>
         </nav>
     );
